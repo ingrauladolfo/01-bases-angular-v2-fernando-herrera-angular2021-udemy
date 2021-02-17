49a2b38a88c92c7352cf1e1d92697c8dfd836f92
Raúl Adolfo Torres Vargas <ingrauladolfo.torresvargas@gmail.com>
2021-02-16 20:16:15 -0600

    Upload the beginning of new course

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+interface Personaje{
+  nombre: string;
+  poder:number;
+}
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.css']
+})
+export class MainPageComponent  {
+  nuevo:Personaje = {
+    nombre:'Trunks',
+    poder: 14000
+  }
+  agregarPersonajes(){
+    console.log(this.nuevo);
+  }
+  cambiarNombre(event:any){
+    console.log(event.target.value)
+  }
+
+} 
